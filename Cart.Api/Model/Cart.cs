c17af8cdcb7f130048dc9bfa1641e7c44b1ce40f
Tomas-Monmany <monmanytomas@gmail.com>
2026-05-11 17:24:59 -0300

    Creacion de ApiCart y Model

---
+++ Cart.Api/Model/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce_G24.Cart.Api.Model
+{
+    public class Cart
+    {
+        public Guid UsuarioId { get; init; }
+        public List<CartItem> Items { get; set; } = new();
+        public DateTime FechadeActualizacion { get; set; }
+    }
+}
---
+++ Cart.Api/Model/CartItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce_G24.Cart.Api.Model
+{
+    public class CartItem
+    {
+        public Guid ProductoId { get; set; }
+        public int Cantidad { get; set; }
+    }
+}

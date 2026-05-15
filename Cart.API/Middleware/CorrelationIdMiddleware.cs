20e16e9df68cec6dbbf4bc115791831558790147
Tomas-Monmany <monmanytomas@gmail.com>
2026-05-15 11:35:40 -0300

    Aplicacion de los CorrelationId. Para esto se creo la carpeta Middleware junto con la clase CorrelationIdMiddleware Y en la carpeta Services se agrego la clase CorrelationIdDelegationHandler

---
+++ Cart.API/Middleware/CorrelationIdMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Serilog.Context;
+namespace ECommerce_G24.Cart.API.Middleware
+{
+    // Middleware para generar o reutilizar el X-Correlation-Id.
+    public class CorrelationIdMiddleware
+    {
+        private const string HeaderName = "X-Correlation-Id";
+
+        private readonly RequestDelegate _next;
+
+        public CorrelationIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Si el request ya viene con X-Correlation-Id,se usa ese sino generamos uno nuevo.
+            var correlationId = context.Request.Headers.TryGetValue(HeaderName, out var value)
+                ? value.ToString()
+                : Guid.NewGuid().ToString();
+
+            // Lo agregamos a la respuesta.
+            context.Response.Headers[HeaderName] = correlationId;
+
+            // Lo agregamos al contexto de Serilog para que aparezca en los logs.
+            using (LogContext.PushProperty("CorrelationId", correlationId))
+            {
+                await _next(context);
+            }
+        }
+    }
+}

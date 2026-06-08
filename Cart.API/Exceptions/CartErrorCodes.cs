bc0a98842071c2f8d17079a4841bd5e4ced0d924
Tomas-Monmany <monmanytomas@gmail.com>
2026-06-08 10:19:42 -0300

    Agregar CartErrorCodes para documentar tipo de errores

---
+++ Cart.API/Exceptions/CartErrorCodes.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce_G24.Cart.API.Exceptions
+{
+    // Catálogo centralizado de errores de Cart.API.
+ 
+    public static class CartErrorCodes
+    {
+        public const string CartNotFound = "CRT-001";
+        public const string ProductNotFound = "CRT-002";
+        public const string InsufficientStock = "CRT-003";
+        public const string InvalidQuantity = "CRT-004";
+        public const string InternalCartError = "CRT-005";
+    }
+}

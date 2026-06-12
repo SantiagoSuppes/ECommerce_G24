6c6086bb25b8a70b765bb32cf0695914cba66a06
Tomas-Monmany <monmanytomas@gmail.com>
2026-06-11 23:56:37 -0300

    Se agrega a las exceptions un catalogo de errores

---
+++ Users.API/Exceptions/UserErrorCodes.cs
@@ -0,0 +1,19 @@
+﻿namespace Users.API.Exceptions;
+
+/// <summary>
+/// Catálogo de códigos de error definido para Users.API.
+/// </summary>
+public static class UserErrorCodes
+{
+    public const string DuplicateEmail = "USR-001";
+
+    public const string InvalidUserData = "USR-002";
+
+    public const string InvalidCredentials = "USR-003";
+
+    public const string BlockedByFailedAttempts = "USR-004";
+
+    public const string BlockedByFraud = "USR-005";
+
+    public const string InternalUserError = "USR-006";
+}
